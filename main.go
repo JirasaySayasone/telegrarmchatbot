dfc36ef96f39b59a492b02999d0e16dbfdcef79b
JirasaySayasone <jirasay04@gmail.com>
2026-02-09 15:08:34 +0700

    token separation

--- main.go
+++ main.go
@@ -1,12 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+// Send any text message to the bot after the bot has been started
 
 func main() {
-	fmt.Println("Hello World")
+	filePath := "C:/Users/Advice/Desktop/project/Telegram_ChatBot/telegrarmchatbot/Token.txt"
+	contentBytes, err := os.ReadFile(filePath) // Read the file into a byte slice
+	if err != nil {
+		log.Fatalf("unable to read file: %v", err)
+	}
+	Token := string(contentBytes)
 
-	TOKEN := "8419395833:AAHpO5ZfslRHH7NeYoClcIEeuo1Gu90Dd4Q"
-	fmt.Println(TOKEN)
-	BOT_USERNAME := "@LaoItDev_MeetingBot"
-	fmt.Println(BOT_USERNAME)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(Token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, handler)
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+}
+
+func startHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "ສະບາຍດີ",
+	})
 }
